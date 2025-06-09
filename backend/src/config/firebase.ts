aa64d9fe2c6dadcf936d8821d3a6ddbe3475d370
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-09 23:13:44 +0100

    Deploiemenet v1.11

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -4,10 +4,10 @@ import { getStorage, connectStorageEmulator } from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NODE_ENV === "production" 
-    ? "plateforme-bim-c779e.firebaseapp.com" 
+    ? "wivision-1b106.firebaseapp.com" 
     : "localhost",
-  projectId: "plateforme-bim-c779e",
-  storageBucket: "plateforme-bim-c779e.appspot.com",
+  projectId: "wivision-1b106",
+  storageBucket: "wivision-1b106.appspot.com",
 };
 
 const app = initializeApp(firebaseConfig);
