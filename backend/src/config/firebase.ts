02104911872af2dc9249dd1963ba1570a6a7197b
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-10 21:57:41 +0100

    Deploiemenet v4.5

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -3,21 +3,10 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  authDomain: "wivision-1b106.firebaseapp.com",
+  projectId: "wivision-1b106",
+  storageBucket: "wivision-1b106.firebasestorage.app", 
 };
-
-// Vérifiez que toutes les variables sont définies
-console.log("Configuration Firebase:", {
-  apiKey: !!firebaseConfig.apiKey,
-  authDomain: !!firebaseConfig.authDomain,
-  projectId: !!firebaseConfig.projectId,
-  storageBucket: !!firebaseConfig.storageBucket
-});
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
