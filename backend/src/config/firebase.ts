73d382c54d57bf6842866c823725028e2a5ab1c5
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-10 20:58:02 +0100

    Deploiemenet v1.12345

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,21 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getStorage, connectStorageEmulator } from "firebase/storage";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NODE_ENV === "production" 
-    ? "wivision-1b106.firebaseapp.com" 
-    : "localhost",
+  authDomain: "wivision-1b106.firebaseapp.com",
   projectId: "wivision-1b106",
- // storageBucket: "wivision-1b106.appspot.com",
+  storageBucket: "wivision-1b106.appspot.com", // ✅ Obligatoire pour Firebase Storage
 };
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
-
-if (process.env.NODE_ENV === "development" && typeof window !== "undefined") {
-  connectStorageEmulator(storage, "127.0.0.1", 4001);
-  console.log('🔥 Firebase Storage Emulator: ACTIF');
-}
 
 export default storage;
