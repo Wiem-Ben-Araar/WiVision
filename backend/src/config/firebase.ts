961f5bfc5bc96e5b8eeab05755cec5366c7a7a80
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-10 22:27:53 +0100

    Deploiemenet v1.1234

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -5,7 +5,9 @@ const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "wivision-1b106.firebaseapp.com",
   projectId: "wivision-1b106",
-  storageBucket: "wivision-1b106.firebasestorage.app", 
+  storageBucket: "wivision-1b106.appspot.com", // Changed from .firebasestorage.app
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
