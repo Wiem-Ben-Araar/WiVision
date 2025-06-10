e670c8dddb676f3dc2c08928aa63fc3f02b5e579
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-10 22:09:41 +0100

    Deploiemenet v1.4.5

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "wivision-1b106.firebaseapp.com",
   projectId: "wivision-1b106",
   storageBucket: "wivision-1b106.firebasestorage.app", 
