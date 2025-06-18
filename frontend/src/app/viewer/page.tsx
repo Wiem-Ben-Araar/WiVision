ae2a4761606f7a05bc187fa220e030b480865eb7
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-18 23:26:36 +0100

    Fixation wasm last 4

--- frontend/src/app/viewer/page.tsx
+++ frontend/src/app/viewer/page.tsx
@@ -188,7 +188,7 @@ function ViewerPageContent() {
         renderer.shadowMap.enabled = true
         renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
-        await viewer.IFC.setWasmPath("/wasm/")
+        await viewer.IFC.setWasmPath("/wasm")
         viewer.clipper.active = true
         viewer.IFC.loader.ifcManager.applyWebIfcConfig({
           COORDINATE_TO_ORIGIN: true,
