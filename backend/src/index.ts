1d2dcc77dd670f1987489c77b2c053d01dac79ff
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-18 21:37:22 +0100

    Fixation cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -39,7 +39,10 @@ app.use(passport.initialize());
 
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: [
+    'http://localhost:3000',
+    'https://wi-vision.vercel.app'  
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
