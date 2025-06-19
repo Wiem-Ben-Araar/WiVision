a85827205f1f8949ada8c2bc0f07cca721197cf3
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-19 16:04:32 +0100

    next one

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,47 +1,17 @@
-// next.config.js
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  staticPageGenerationTimeout: 300,
+  staticPageGenerationTimeout: 60,
   images: {
-    domains: ["lh3.googleusercontent.com"],
-    unoptimized: true,
+    domains: ["lh3.googleusercontent.com"], 
   },
-  output: "standalone",
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-
-  webpack: (config) => {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-      topLevelAwait: true,
-    };
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-      crypto: false,
-    };
-    return config;
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/wasm/:path*",
-        headers: [
-          { key: "Content-Type", value: "application/wasm" },
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-        ],
-      },
-    ];
-  },
-
   async rewrites() {
     return [
+      {
+        source: "/_next/static/chunks/wasm/web-ifc.wasm",
+        destination: "/wasm/web-ifc.wasm",
+      },
       {
         source: "/api/:path*",
         destination: "https://wivision.onrender.com/api/:path*",
