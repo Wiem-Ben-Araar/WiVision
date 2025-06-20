cfd75220dd5ca97bd87315cb2fb6e76d2e6f61c4
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-20 13:30:46 +0100

    last one

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,15 +6,39 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"], 
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
   async rewrites() {
     return [
       {
-        source: "/_next/static/chunks/wasm/web-ifc.wasm",
-        destination: "/wasm/web-ifc.wasm",
-      },
-      {
         source: "/api/:path*",
         destination: "https://wivision.onrender.com/api/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
       },
     ];
   },
