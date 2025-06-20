bfde00bbd10550b9f951178eb7cc4160ecfd4d7a
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-20 14:46:57 +0100

    wasm/

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,11 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   staticPageGenerationTimeout: 60,
+  images: {
+    domains: ["lh3.googleusercontent.com"], 
+  },
   async rewrites() {
     return [
       {
-        source: '/_next/static/chunks/wasm/web-ifc.wasm', // Chemin demandé par Web-IFC
-        destination: '/wasm/web-ifc.wasm', // Fichier réellement présent dans public/wasm/
+        source: "/_next/static/chunks/wasm/web-ifc.wasm",
+        destination: "/wasm/web-ifc.wasm",
+      },
+      {
+        source: "/api/:path*",
+        destination: "https://wivision.onrender.com/api/:path*",
       },
     ];
   },
