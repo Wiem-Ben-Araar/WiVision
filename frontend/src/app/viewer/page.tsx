735184f61687f069e2e93e2fd6c93a4f45025243
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-20 15:40:23 +0100

    log viwer

--- frontend/src/app/viewer/page.tsx
+++ frontend/src/app/viewer/page.tsx
@@ -189,12 +189,14 @@ function ViewerPageContent() {
         renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
         await viewer.IFC.setWasmPath("/wasm/")
-        
+        console.log("✅ WASM path set to /wasm/");
         
         viewer.IFC.loader.ifcManager.applyWebIfcConfig({
           COORDINATE_TO_ORIGIN: true,
           USE_FAST_BOOLS: false,
         })
+        console.log("🧠 IFC Manager:", viewer.IFC.loader.ifcManager);
+console.log("🧩 wasmModule:", viewer.IFC.loader.ifcManager.wasmModule);
 viewer.clipper.active = true
         // Gestionnaire de clic
         if (containerRef.current) {
