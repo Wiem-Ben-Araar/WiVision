8ab141a9c95508ccac5bb6b606ec6f0363916d57
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-06-23 13:45:07 +0100

    NEW solution with route

--- frontend/src/app/wasm/[...path]/route.ts
+++ frontend/src/app/wasm/[...path]/route.ts
@@ -1,5 +1,4 @@
 // app/wasm/[...path]/route.ts
-
 import { join } from 'path';
 import { readFile } from 'fs/promises';
 import { stat } from 'fs/promises';
@@ -10,19 +9,17 @@ export async function GET(request: Request, { params }: { params: { path: string
   const filePath = join(process.cwd(), 'public', 'wasm', ...params.path);
   
   try {
-    await stat(filePath); // Vérifie que le fichier existe
-    
+    await stat(filePath);
     const file = await readFile(filePath);
     const headers = new Headers();
     
-    // Headers essentiels pour WASM
     headers.set('Content-Type', 'application/wasm');
     headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
     headers.set('Cross-Origin-Opener-Policy', 'same-origin');
     headers.set('Cache-Control', 'public, max-age=31536000, immutable');
     
     return new Response(file, { headers });
-  } catch (error) {
+  } catch {
     return new Response('WASM file not found', { status: 404 });
   }
 }
