5db396f8275b253a4aa949672f0acebfb8ba6974
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2025-07-07 00:01:58 +0100

    profile et seeting

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+
 import { useAuth } from "@/hooks/use-auth";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
